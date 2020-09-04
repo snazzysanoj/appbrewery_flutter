280055545d8eb8b8ec30e28dc33479c0394d0508
Sanoj Kumar R <sanoj@strongboxit.com>
2020-09-04 12:53:22 +0530

    gradle enableR8

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(CardApp());
+}
+
+class CardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.red,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
