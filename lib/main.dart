70416a18aad6b776725d2aa3ec268dd99a10a6ba
Sanoj Kumar R <sanoj@strongboxit.com>
2020-09-05 22:42:54 +0530

    Step 3 - Create a Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: BallPage(),
+        home: Ball(),
       ),
     );
 
@@ -17,6 +17,32 @@ class BallPage extends StatelessWidget {
           leading: Icon(Icons.email),
           title: Text('Ask Me Anything'),
           backgroundColor: Colors.blue[900],
+        ),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          leading: Icon(Icons.email),
+          title: Text('Ask Me Anything !'),
+        ),
+        body: Center(
+          child: Image.asset('images/ball1.png'),
         ),
       ),
     );
