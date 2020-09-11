a736a9b8d1941fba2e308ad412f3ef131eafcdde
Sanoj Kumar R <sanoj@strongboxit.com>
2020-09-12 00:34:53 +0530

    Final - all challenge completed

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,12 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future<String> makeGET(
+      {String currency = 'USD', String crypto = 'BTC'}) async {
+    String url =
+        'https://rest.coinapi.io/v1/exchangerate/$crypto/$currency?apikey=BA6255CB-B373-46D7-94D4-695E6B533820';
+    var t = await http.get(url);
+    return jsonDecode(t.body)['rate'].toString();
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
+var cryptPrice = {'btc': '0', 'eth': '0', 'ltc': '0'};
+
 class _PriceScreenState extends State<PriceScreen> {
+  String currentCurrency = 'USD';
+  CoinData coinData = CoinData();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,35 +22,78 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
+          Column(
+              children: cryptoList.map<TickerWidget>((String crypto) {
+            return TickerWidget(
+                price: cryptPrice[crypto],
+                currentCrypt: crypto,
+                currentCurrency: currentCurrency);
+          }).toList()),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Center(
+              child: DropdownButton(
+                value: currentCurrency,
+                items: currenciesList
+                    .map<DropdownMenuItem<String>>((String currency) {
+                  return DropdownMenuItem<String>(
+                    value: currency,
+                    child: Text(currency),
+                  );
+                }).toList(),
+                onChanged: (String newCurrency) async {
+                  currentCurrency = newCurrency;
+                  for (String cryptoName in cryptoList) {
+                    cryptPrice[cryptoName] = await coinData.makeGET(
+                        currency: currentCurrency, crypto: cryptoName);
+                  }
+                  setState(null);
+                },
+              ),
+            ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class TickerWidget extends StatelessWidget {
+  const TickerWidget({
+    Key key,
+    @required this.price,
+    @required this.currentCurrency,
+    @required this.currentCrypt,
+  }) : super(key: key);
+
+  final String price;
+  final String currentCrypt;
+  final String currentCurrency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $currentCrypt = $price $currentCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
