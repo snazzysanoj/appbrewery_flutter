ddd112b5ec3d3a782ee3d6ef5a701dfe4c99b861
Sanoj Kumar R <sanoj@strongboxit.com>
2020-09-14 12:46:04 +0530

    Loginscren logo hotfix

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -18,7 +18,7 @@ class _LoginScreenState extends State<LoginScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            LogoProvider(size: logoSize.small),
+            LogoProvider(size: logoSize.large),
             SizedBox(
               height: 48.0,
             ),
