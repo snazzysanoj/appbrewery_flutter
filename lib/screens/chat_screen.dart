1cde75b907685359a7e5f681db96e4ea085c252a
Sanoj Kumar R <sanoj@strongboxit.com>
2020-09-19 23:11:50 +0530

    Working Login/Register with Spinner

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String route = 'ChatScreen';
@@ -8,6 +9,28 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _auth = FirebaseAuth.instance;
+  FirebaseUser user;
+
+  void getCurrentUser() async {
+    final _user = await _auth.currentUser();
+    try {
+      if (_user != null) {
+        user = _user;
+      } else{
+        Navigator.pop(context);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +40,8 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                //Implement logout functionality
+                _auth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String route = 'RegistrationScreen';
@@ -8,41 +11,66 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email, password;
+  final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            LogoProvider(size: logoSize.large),
-            SizedBox(
-              height: 48.0,
-            ),
-            InputTextBox(
-              text: 'Enter your email',
-              onChanged: (value) {},
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            InputTextBox(
-              text: 'Enter your password',
-              onChanged: (value) {},
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            LoginRegisterButtons(
-              text: 'Log In',
-              onPressed: () {
-                print("register?");
-              },
-            ),
-          ],
+    return LoadingOverlay(
+      isLoading: showSpinner,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              LogoProvider(size: logoSize.large),
+              SizedBox(
+                height: 48.0,
+              ),
+              InputTextBox(
+                text: 'Enter your email',
+                onChanged: (value) => email = value,
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              InputTextBox(
+                text: 'Enter your password',
+                onChanged: (value) => password = value,
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              LoginRegisterButtons(
+                text: 'Register',
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  print("register?");
+                  print("$email,$password register");
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+
+                    setState(() {
+                      showSpinner = false;
+                    });
+
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.route);
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
