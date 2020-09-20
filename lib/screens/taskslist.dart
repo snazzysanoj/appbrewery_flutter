16310b01da2c0bb1934257a68281ba740cc48d2d
Sanoj Kumar R <sanoj@strongboxit.com>
2020-09-20 14:59:54 +0530

    AddTask Refactor and Scrollable with keyboard

--- lib/screens/taskslist.dart
+++ lib/screens/taskslist.dart
@@ -1,59 +1,5 @@
 import 'package:flutter/material.dart';
-
-class AddTaskPopup extends StatelessWidget {
-  const AddTaskPopup({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey[600],
-      child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(25),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text('Add Task',
-                        style: TextStyle(
-                          color: Colors.lightBlueAccent,
-                          fontSize: 30,
-                          // fontWeight: FontWeight.bold,
-                        )),
-                  ),
-                  Divider(thickness: 3, color: Colors.lightBlueAccent),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Enter Task Name',
-                      border: InputBorder.none,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-              FlatButton(
-                padding: EdgeInsets.all(20),
-                color: Colors.lightBlueAccent,
-                onPressed: () {},
-                child: Text(
-                  'Add Task',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              )
-            ],
-          )),
-    );
-  }
-}
+import 'AddTask.dart';
 
 class TasksList extends StatelessWidget {
   static const String route = "taskslist";
@@ -66,7 +12,13 @@ class TasksList extends StatelessWidget {
         backgroundColor: Colors.lightBlue,
         floatingActionButton: FloatingActionButton(
           onPressed: () => showModalBottomSheet(
-              context: context, builder: (context) => AddTaskPopup()),
+            context: context,
+            builder: (context) => SingleChildScrollView(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: AddTaskPopup()),
+            isScrollControlled: true,
+          ),
           child: Icon(Icons.add),
         ),
         body: SafeArea(
